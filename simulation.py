1ef388c448f1d2ca2663b02e2837d1bfc2f9867e
BlackFalcon <90463234+Ejderhalp@users.noreply.github.com>
2026-04-20 01:03:22 +0000

    Sun, Apr 19, 2026, 9:03 PM -04:00

--- simulation.py
+++ simulation.py
@@ -4,31 +4,14 @@ import random
 import numpy as np
 from itertools import combinations
 import plotly.graph_objects as go
+from simulation_lib import get_numerical_input, get_coordinate_input
 
 
 xs = []
 os = []
 past_choices =[]
-def get_numerical_input():
-    while True:
-        choice = int(input("What move do you want to make? "))
-        if choice in past_choices:
-            print("No doubling up guesses!")
-        else:
-            break
-    past_choices.append(choice)
-    return choice
-def get_coordinate_input():
-    while True:
-        choice = input("What move do you want to make? (separated by spaces) ").split()
-        for i in range(4):
-            choice[i] = int(choice[i])
-        if choice in past_choices:
-            print("No doubling up guesses!")
-        else:
-            break
-    past_choices.append(choice)
-    return choice
+
+
 
 def update_xs(choice):
     cube_number = choice//27 #outputs 0, 1 or 2 (the w coordinate)
---
+++ simulation_lib.py
@@ -0,0 +1,25 @@
+
+
+past_choices =[]
+
+def get_numerical_input():
+    while True:
+        choice = int(input("What move do you want to make? "))
+        if choice in past_choices:
+            print("No doubling up guesses!")
+        else:
+            break
+    past_choices.append(choice)
+    return choice
+
+def get_coordinate_input():
+    while True:
+        choice = input("What move do you want to make? (separated by spaces) ").split()
+        for i in range(4):
+            choice[i] = int(choice[i])
+        if choice in past_choices:
+            print("No doubling up guesses!")
+        else:
+            break
+    past_choices.append(choice)
+    return choice
